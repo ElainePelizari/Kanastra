ab739ca39b09d7eab9062a2938580de2623b4fc9
Elaine Pelizari <elaine.pelizari95@gmail.com>
2023-02-14 17:21:42 -0300

    Development: Front preparado pra receber o arquivo

--- resources/js/Pages/Tickets/Show.vue
+++ resources/js/Pages/Tickets/Show.vue
@@ -1,4 +1,16 @@
 <script setup>
+import { useForm } from '@inertiajs/vue3'
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+
+const form = useForm({
+    file: null,
+})
+
+function submit() {
+    form.post('/upload')
+}
 </script>
 
 <template>
@@ -12,6 +24,24 @@
             <div class="mt-6 text-gray-500">
                 Aqui você irá importar seu arquivo CSV para que possamos gerar o seu boleto!
             </div>
+
+            <form @submit.prevent="submit">
+                <div class="mt-10">
+                    <input
+                        type="file"
+                        ref="file"
+                        @input="form.file = $event.target.files[0]"
+                    >
+                </div>
+
+                <progress v-if="form.progress" :value="form.progress.percentage" max="100">
+                    {{ form.progress.percentage }}%
+                </progress>
+
+                <PrimaryButton class="mt-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                    Enviar
+                </PrimaryButton>
+            </form>
         </div>
     </div>
 </template>
